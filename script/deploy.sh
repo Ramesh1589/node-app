1c0c99123df09e264cc9c3429512b72e966d3c75
rameshrathodbv <ramesh.rathod@brainvire.com>
2020-02-05 12:45:15 +0530

    Added Deployment Script file

---
+++ script/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ssh ubuntu@52.66.183.73 
+    cd ~/project
+    git pull origin master
+    nvm install v10.11.0
+    npm install
+    npm install -g nodemon pm2
+    npm start
+ # pm2 restart ecosystem.config.js
+    exit
